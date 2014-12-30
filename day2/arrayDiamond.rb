4b17a665e085cfe3d264f765c9faa6711350e6c3
showingWarf91 <joshi.anujo7@gmail.com>
2014-12-30 18:20:20 +0545

    correction: deleting the array by use of index instead of value

--- day2/arrayDiamond.rb
+++ day2/arrayDiamond.rb
@@ -1,42 +1,48 @@
-def functionMy(b, value)
+PEOPLE_COUNT = 3
+
+def valueDistribution(given_array, possible_value)
   len = 0
   new_array = []
   new_array_size = 0
-  while (len < b.length )
-    if(b[len] + new_array_size <= value)
-      new_array.push(b[len])
-      new_array_size += b[len]
-      b.delete(b[len])
-      len = len-1
+  while (len < given_array.length )
+    if(given_array[len] + new_array_size <= possible_value)
+      new_array.push(given_array[len])
+      new_array_size += given_array[len]
+      given_array.delete_at(len)
+      len = len - 1
     end
-    len += 1
+    len = len + 1
+
   end
   new_array
 end
 
 #a = [4,5,6,8,1,3]
-a = [1,2,3,4,5,6,7,8]
-sum = a.inject{ |sum = 0,a |sum + a}
-n = 3
+#a=[1,2,3,4,5]
+a = [2,2,2,2,1,1,3,2]
+array_sum = a.inject{ |sum, a| sum + a}
+
 flag = true
-if(sum%n == 0)
+if(array_sum%PEOPLE_COUNT == 0)
   b = a.sort.reverse
   count = 1
-  total_sum = sum / n
-  array = []
-  while(count <= n)
-    array[count] = functionMy(b, total_sum)
-    if (array[count] == [])
+  possible_sum = array_sum / PEOPLE_COUNT
+  distribute_array = []
+  while(count <= PEOPLE_COUNT)
+    distribute_array[count] = valueDistribution(b, possible_sum)
+    if (distribute_array[count] == [])
       flag = false
     end
     count += 1
   end
   if(flag == true)
     count = 1
-    while(count <=n )
-      print "#{count} Array: #{array[count].sort} \n"
+    while(count <= PEOPLE_COUNT )
+      print "#{count} Array: #{distribute_array[count].sort} \n"
       count += 1
     end
+  else
+    print "Invalid Array"
   end
 else
   print "Invalid array"
